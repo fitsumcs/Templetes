bd76c1a58d874ef0b48d82c484de3b50e1cc6934
3020 <fitsumcs@gmail.com>
2020-07-28 16:14:53 +0300

    Adding background image

--- Wellcom_Page/app.js
+++ Wellcom_Page/app.js
@@ -1,5 +1,7 @@
 // doc 
 const time = document.querySelector('#time');
+const greeting = document.querySelector('#greeting');
+
 
 
 
@@ -24,4 +26,28 @@ function addZero(time) {
     let addZerov = (parseInt(time) < 10 ? '0' : '') + time;
     return addZerov;
 }
+
+function getGreeting() {
+    const hr = new Date().getHours();
+
+    if (hr < 12) {
+        // Goo Morning 
+        document.body.style.backgroundImage = "url('./img/morning.jpg')";
+        greeting.textContent = "Good Morning";
+
+
+    } else if (hr < 18) {
+        // Good AfterNoon
+        document.body.style.backgroundImage = "url('./img/afternoon.jpg')";
+        greeting.textContent = "Good Afternoon";
+
+
+    } else {
+        // Good Evening 
+        document.body.style.backgroundImage = "url('./img/evening.jpg')";
+        greeting.textContent = "Good Evening";
+
+    }
+}
 getTime();
+getGreeting();
