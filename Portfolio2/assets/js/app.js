a2db7858ee53ede2c39a7cc30ed18a95888dea6d
3020 <fitsumcs@gmail.com>
2020-08-04 14:42:53 +0300

    Adding event listener on each link on portfolio2

--- Portfolio2/assets/js/app.js
+++ Portfolio2/assets/js/app.js
@@ -2,6 +2,8 @@
 const open = document.querySelector('.open');
 const close = document.querySelector('.close');
 const menu = document.querySelector('.nav-list');
+const nav_link = document.querySelectorAll('.nav-item a');
+
 
 open.addEventListener('click', () => {
 
@@ -11,3 +13,9 @@ close.addEventListener('click', () => {
 
     menu.classList.remove('active');
 });
+nav_link.forEach((item) => {
+    item.addEventListener('click', () => {
+
+        menu.classList.remove('active');
+    });
+})
